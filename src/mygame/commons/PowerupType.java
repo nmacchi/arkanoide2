3cd5e54ebfae79043666b4cdfce129a4784de626
Nicolas Macchi <nmacchi1973@gmail.com>
2017-02-02 23:59:51 -0300

    refactoring

--- src/mygame/commons/PowerupType.java
+++ src/mygame/commons/PowerupType.java
@@ -10,21 +10,21 @@ package mygame.commons;
  */
 public enum PowerupType {
     
-    FIREBALL(0, "Fireball", ""),
-    EXTRA_BALLS(1, "Add extra balls", ""),
-    LIFE(2, "Life", ""),
-    SLOWER(3, "Decrease ball speed", ""),
-    FIRE(4, "Convert ball into fire", "");
+    FIREBALL(0, "Fireball", "Textures/pill/pill_texture_blue.png"),
+    EXTRA_BALLS(1, "Add extra balls", "Textures/pill/pill_texture_light_blue.png"),
+    LIFE(2, "Life", "Textures/pill/pill_texture_gray.png"),
+    SLOWER(3, "Decrease ball speed", "Textures/pill/pill_texture_green.png"),
+    FIRE(4, "Convert ball into fire", "Textures/pill/pill_texture_red.png");
     
     private final int codigo;
     private final String description;
-    private final String textureName;
+    private final String texture;
 
     
-    PowerupType(int codigo, String description, String textureName){
+    PowerupType(int codigo, String description, String texture){
         this.codigo = codigo;
         this.description = description;
-        this.textureName = textureName;
+        this.texture = texture;
     }
 
     public int getCodigo() {
@@ -33,6 +33,10 @@ public enum PowerupType {
 
     public String getDescription() {
         return description;
+    }
+
+    public String getTexture() {
+        return texture;
     }
     
     
